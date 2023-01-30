8b4e42641bfc9f198e14ad40f294bb48b8854a84
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-01-30 19:59:12 -0300

    desafio 1 concluído

--- src/challenges.js
+++ src/challenges.js
@@ -1,19 +1,65 @@
 // Desafio 1 - Crie a função compareTrue
 
+function compareTrue(boolean1, boolean2) {
+  if (boolean1 === true && boolean2 === true) {
+    return true;
+  } return false;
+}
+
 // Desafio 2 - Crie a função splitSentence
 
 // Desafio 3 - Crie a função concatName
 
+/* function concatName() {
+  let ultimoItem = concatName.length - 1;
+  let primeiroItem = concatName[0];
+  return ultimoItem, primeiroItem;
+} */
+
 // Desafio 4 - Crie a função footballPoints
 
-function footballPoints (wins, ties) {
+/* function footballPoints (wins, ties) {
   let resultado = wins * 3 + ties * 1
   return resultado
-}
+} */
 
 // Desafio 5 - Crie a função highestCount
 
+/* function highestCount (array) {
+  let Count = 0;
+  let maiorNumero = array[0];
+
+  for (let index = 0; index < arguments.length; index += 1) {
+    if (arguments[index] > Count) {
+      count = arguments[i];
+    }
+  }
+  return count
+} */
+
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
+
+/* function calcTriangleArea (base, height) {
+  return base * height / 2
+}
+function calcRectangleArea (base, height) {
+  return base * height
+}
+function calcAllAreas (base, height, form) {
+  let retorno = ''
+  switch (form) {
+    case 'triangulo':
+      retorno = "O valor da área do triângulo é de:" calcTriangleArea(base, height)
+      break
+    case 'retângulo':
+      retorno = "O valor da área do retângulo é de:" calcRectangleArea(base, height)
+      break
+    default
+      retorno = "Não foi possível fazer o cálculo, insira uma forma geométrica válida"
+      break
+  }
+}
+return retorno */
 
 // Desafio 7 - Crie a função catAndMouse
 
@@ -25,17 +71,20 @@ function footballPoints (wins, ties) {
 
 // Não modifique essas linhas
 module.exports = {
-  calcTriangleArea: typeof calcTriangleArea === 'function' ? calcTriangleArea : (() => {}),
-  calcRectangleArea: typeof calcRectangleArea === 'function' ? calcRectangleArea : (() => {}),
-  calcAllAreas: typeof calcAllAreas === 'function' ? calcAllAreas : (() => {}),
-  catAndMouse: typeof catAndMouse === 'function' ? catAndMouse : (() => {}),
-  compareTrue: typeof compareTrue === 'function' ? compareTrue : (() => {}),
-  concatName: typeof concatName === 'function' ? concatName : (() => {}),
-  decode: typeof decode === 'function' ? decode : (() => {}),
-  encode: typeof encode === 'function' ? encode : (() => {}),
-  fizzBuzz: typeof fizzBuzz === 'function' ? fizzBuzz : (() => {}),
-  footballPoints: typeof footballPoints === 'function' ? footballPoints : (() => {}),
-  highestCount: typeof highestCount === 'function' ? highestCount : (() => {}),
-  splitSentence: typeof splitSentence === 'function' ? splitSentence : (() => {}),
-  techList: typeof techList === 'function' ? techList : (() => {}),
+  calcTriangleArea:
+    typeof calcTriangleArea === 'function' ? calcTriangleArea : () => {},
+  calcRectangleArea:
+    typeof calcRectangleArea === 'function' ? calcRectangleArea : () => {},
+  calcAllAreas: typeof calcAllAreas === 'function' ? calcAllAreas : () => {},
+  catAndMouse: typeof catAndMouse === 'function' ? catAndMouse : () => {},
+  compareTrue: typeof compareTrue === 'function' ? compareTrue : () => {},
+  concatName: typeof concatName === 'function' ? concatName : () => {},
+  decode: typeof decode === 'function' ? decode : () => {},
+  encode: typeof encode === 'function' ? encode : () => {},
+  fizzBuzz: typeof fizzBuzz === 'function' ? fizzBuzz : () => {},
+  footballPoints:
+    typeof footballPoints === 'function' ? footballPoints : () => {},
+  highestCount: typeof highestCount === 'function' ? highestCount : () => {},
+  splitSentence: typeof splitSentence === 'function' ? splitSentence : () => {},
+  techList: typeof techList === 'function' ? techList : () => {},
 };
